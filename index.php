65e59e2b7ba7a3773b43877cd70aeddfa651d137
John Lubinski <jwlubinski@gmail.com>
2017-02-13 10:58:18 -0500

    Added files from htdocs to Git repo

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Assignment 1</title>
+    <meta charset="utf-8" />
+    <meta name=viewport content="width=device-width, initial-scale=1" />
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Coda" />
+    <script src="js/bootstrap.min.js"></script>
+</head>
+
+<div class="container-fluid">
+    <div id="helloworld">
+        <div class="row">
+            <div class="col-sm-8">
+                <h1>John W. Lubinski</h1>
+                <a href="images/me.png" title="me">
+                <h1>About Me</h1>
+                <p>I am a full time student taking this course following CSCI E-10 and CSCI E-50 (CS50) to solidify and build upon my knowledge of web application programming. I am hoping to apply and get into Harvard Extension School's ALB program following this semester.</p>
+                <h1>Random Quote</h1>
+                <blockquote>There is no such thing as inner peace. There is only nervousness or death. Any attempt to prove otherwise constitutes unacceptable behavior. -Fran Lebowitz</blockquote>
+            </div>
+        </div>
+    </div>
+</div>
